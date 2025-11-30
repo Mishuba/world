8b40ae37bb01fcae0ead690f61fad12f1e852fd9
Mishuba <mishuba14@gmail.com>
2025-11-30 13:50:28 -0500

    Update TfLive.sh

--- hls/scripts/TfLive.sh
+++ hls/scripts/TfLive.sh
@@ -1,1 +1,22 @@
 # placeholder
+
+#!/bin/bash
+
+INPUT_STREAM="rtmp://localhost/live/$1"   # dynamic stream name from Nginx exec_static
+LOGFILE="/var/log/ffmpeg-restream.log"
+
+# Targets
+# TWITCH_URL="rtmp://live.twitch.tv/app/$TWITCH_KEY"
+YOUTUBE_URL="rtmp://a.rtmp.youtube.com/live2/3egr-4vfq-56yj-amtg-e7v1"
+# INSTAGRAM_URL="rtmp://rtmp.instagram.com:80/rtmp/$INSTAGRAM_KEY"
+
+echo "$(date) Starting FFmpeg restream for stream: $1" >> "$LOGFILE"
+
+# Run FFmpeg
+/usr/bin/ffmpeg -i "$INPUT_STREAM" \
+    -c:v copy -c:a copy -f flv "$TWITCH_URL" \
+    -c:v copy -c:a copy -f flv "$YOUTUBE_URL" \
+    -c:v copy -c:a copy -f flv "$INSTAGRAM_URL" \
+    >> "$LOGFILE" 2>&1
+
+echo "$(date) FFmpeg exited for stream: $1" >> "$LOGFILE"
