992c334ea93c3954c09fd293326f29894b6fab31
Mishuba <mishuba14@gmail.com>
2026-01-04 09:12:22 -0500

    Update tfMain.php

--- tfMain.php
+++ tfMain.php
@@ -32,6 +32,8 @@ if (isApiRequest()) {
         header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
         header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
         header("Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Requested-With");
+
+header("Access-Control-Allow-Credentials: true");
     }
     if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
         http_response_code(200);
