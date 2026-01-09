a3b425b0f9c31674673c3efb8d35e6fe97398884
Mishuba <mishuba14@gmail.com>
2026-01-09 13:56:23 -0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 // --- Required files & namespaces ---
-require_once "Arrays.php";
+//require_once "Arrays.php";
 require_once "config.php";
 require_once __DIR__ . "/stripestuff/vendor/autoload.php";
 
