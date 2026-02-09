4f1ecd79fc555115618841364d05a506e7a8be1d
Mishuba <mishuba14@gmail.com>
2026-02-09 10:01:16 -0500

    Update store.php

--- store.php
+++ store.php
@@ -13,6 +13,10 @@ header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Requested-With");
 header("Content-Type: application/json");
 
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(204);
+    exit;
+}
 // ============================
 // ERROR REPORTING (DEV)
 // ============================
