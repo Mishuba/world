5af81db4196e646ef5502c56c74b97b5072510b0
Mishuba <mishuba14@gmail.com>
2026-02-09 13:10:06 -0500

    Update RadioPlaylist.php

--- RadioPlaylist.php
+++ RadioPlaylist.php
@@ -15,8 +15,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     exit;
 }
 
+require_once __DIR__ . "/config.php";
 require_once __DIR__ . "/functions.php";
-require "/vendor/autoload.php";
+require_once __DIR__ . "/vendor/autoload.php";
 
 use Aws\Exception\AwsException;
 use Aws\Credentials\Credentials;
