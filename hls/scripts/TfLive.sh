8f365960d0c1483e347cb390b4b80fbd1db7a0b9
Mishuba <74330085+Mishuba@users.noreply.github.com>
2026-05-15 14:27:03 -0400

    Update TfLive.sh

--- hls/scripts/TfLive.sh
+++ hls/scripts/TfLive.sh
@@ -8,11 +8,18 @@ YOUTUBE_URL="rtmp://a.rtmp.youtube.com/live2/3egr-4vfq-56yj-amtg-e7v1"
 # TWITCH_URL="rtmp://live.twitch.tv/app/XXXX"
 # INSTAGRAM_URL="rtmp://rtmp.instagram.com:80/rtmp/XXXX"
 
-echo "$(date) Starting FFmpeg for stream: $STREAM_KEY" >> "$LOGFILE"
+echo "$(date) Starting enhanced FFmpeg for stream: $STREAM_KEY" >> "$LOGFILE"
 
-/usr/bin/ffmpeg -loglevel warning -i "$INPUT_STREAM" \
-  -c:v copy -c:a copy \
-  -f flv "$YOUTUBE_URL" \
-  >> "$LOGFILE" 2>&1
+/usr/bin/ffmpeg -loglevel warning \
+-fflags nobuffer \
+-flags low_delay \
+-i "$INPUT_STREAM" \
+-vf "fps=30,hqdn3d=1.5:1.5:6:6,scale=1280:720:flags=lanczos,unsharp=5:5:0.8:3:3:0.4,eq=contrast=1.1:saturation=1.15" \
+-c:v libx264 -preset veryfast -tune zerolatency \
+-b:v 3000k -maxrate 3000k -bufsize 6000k \
+-g 60 -keyint_min 60 \
+-c:a aac -b:a 128k \
+-f flv "$YOUTUBE_URL" \
+>> "$LOGFILE" 2>&1
 
 echo "$(date) FFmpeg exited for stream: $STREAM_KEY" >> "$LOGFILE"
