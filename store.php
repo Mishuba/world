c61ab82de21a1e92d154a217e14feb747602ca08
Mishuba <74330085+Mishuba@users.noreply.github.com>
2026-07-12 16:16:33 -0400

    cool12

--- store.php
+++ store.php
@@ -6,6 +6,10 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 
+header("Access-Control-Allow-Origin: https://tsunamiflow.club");
+header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Requested-With, X-Request-Type");
 header("Content-Type: application/json; charset=utf-8");
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
